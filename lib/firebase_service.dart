b0d0dbca7dae1bf2eff7c90fc5c5819a56a4dc47
ttt-huong <huong251200@gmail.com>
2026-06-02 15:16:00 +0700

    push

--- lib/firebase_service.dart
+++ lib/firebase_service.dart
@@ -13,7 +13,7 @@ class FirebaseService {
       // In ra ID của document vừa tạo để tiện theo dõi khi debug
       print('Đã lưu dữ liệu lên Firestore thành công! Document ID: ${docRef.id}');
     } catch (e) {
-      // In chi tiết lỗi ra console của nhà phát triển
+      // In chi tiết lỗi ra console của nhà phát triểnz
       print('Lỗi xảy ra tại FirebaseService.saveUser: $e');
       
       // Đẩy lỗi ra ngoài để phía Giao diện (UI) có thể catch và hiển thị SnackBar thông báo cho người dùng
